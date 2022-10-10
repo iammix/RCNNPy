aa21181306df00cc0694b9c30081636cd68a4c50
Konstantinos Mixios <k.mixios@gmail.com>
2022-10-10 15:50:06 +0300

    UPDATE

--- rcnn/utilities.py
+++ rcnn/utilities.py
@@ -6,6 +6,11 @@ import numpy as np
 import cv2
 from config import Config
 import utils
+from skimage.measure import find_contours
+import matplotlib.pyplot as plt
+from matplotlib import patches, lines
+from matplotlib.patches import Polygon
+import colorsys
 
 
 class ShapeConfig(Config):
@@ -110,3 +115,87 @@ class ShapesDataset(utils.Dataset):
         keep_ixs = utils.non_max_suppression(np.array(boxes), np.arange(N), 0.3)
         shapes = [s for i, s in enumerate(shapes) if i in keep_ixs]
         return bg_color, shapes
+
+
+def display_images(images, titles=None, cols=4, cmap=None, norm=None, interpolation=None):
+    titles = titles if titles is not None else [""] * len(images)
+    rows = len(images) // cols + 1
+    plt.figure(figsize=(14, 14 * rows // cols))
+    i = 1
+    for image, title in zip(images, titles):
+        plt.subplot(rows, cols, i)
+        plt.title(title, fontsize=9)
+        plt.axis('off')
+        plt.imshow(image.astype(np.uint8), cmap=cmap, norm=norm, interpolation=interpolation)
+        i += 1
+    plt.show()
+
+
+def random_colors(N, bright=True):
+    brightness = 1.0 if bright else 0.7
+    hsv = [(i / N, 1, brightness) for i in range(N)]
+    colors = list(map(lambda c: colorsys.hsv_to_rgb(*c), hsv))
+    random.shuffle(colors)
+    return colors
+
+
+def apply_mask(image, mask, color, alpha=0.5):
+    for c in range(3):
+        image[:, :, c] = np.where(mask == 1, image[:, :, c] * (1 - alpha) + alpha * color[c] * 255, image[:, :, c])
+    return image
+
+
+def display_instances(image, boxes, masks, class_ids, class_names, scores=None, title="", figsize=(16, 16), ax=None,
+                      show_mask=True, show_bbox=True, colors=None, captions=None):
+    N = boxes.shape[0]
+    if not N:
+        print('\n*** No instances to display *** \n')
+    else:
+        assert boxes.shape[0] == masks.shape[-1] == class_ids.shape[0]
+
+    auto_show = False
+    if not ax:
+        _, ax = plt.subplots(1, figsize=figsize)
+        auto_show = True
+
+    colors = colors or random_colors(N)
+    height, width = image.shape[:2]
+    ax.set_ylim(height + 10, -10)
+    ax.set_xlim(-10, width + 10)
+    ax.axis('off')
+    ax.set_title(title)
+
+    masked_image = image.astype(np.uint32).copy()
+    for i in range(N):
+        color = colors[i]
+        if not np.any(boxes[i]):
+            continue
+        y1, x1, y2, x2 = boxes[i]
+        if show_bbox:
+            p = patches.Rectangle((x1, y1), x2 - x1, y2 - y1, linewidth=2, alpha=0.7, linestyle='dashed',
+                                  edgecolor=color, facecolor='none')
+            ax.add_patch(p)
+        if not captions:
+            class_id = class_ids[i]
+            score = scores[i] if scores is not None else None
+            label = class_names[class_id]
+            caption = "{} {:.3f}".format(label, score) if score else label
+        else:
+            caption = captions[i]
+        ax.text(x1, y1 + 8, caption, color='w', size=11, backgroundcolor='none')
+
+        mask = masks[:, :, i]
+        if show_mask:
+            masked_image = apply_mask(masked_image, mask, color)
+
+        padded_mask = np.zeros(
+            (mask.shape[0] + 2, mask.shape[i] + 2), dtype=np.uint8)
+        padded_mask[1:-1, 1:-1] = mask
+        contours = find_contours(padded_mask, 0.5)
+        for verts in contours:
+            verts = np.fliplr(verts) - 1
+            p = Polygon(verts, facecolor='none', edgecolor=color)
+            ax.add_patch(p)
+    ax.imshow(masked_image.astype(np.uint8))
+    if auto_show:
+        plt.show()
